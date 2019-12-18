35ccd2ca3e178679043c6ed9897ee422eee4028a
pdtayl888 <patrickdtaylor1980@gmail.com>
2019-12-17 22:25:16 -0500

    added gitignore

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 //////////////////////////////////////////////////////////////////////////
+// CHECK PWNED FOR PASSWORD BREACHES********
 
 app.get('/api/checkpwned', async (req, res) => {
   const hash = crypto.createHash('sha1');
@@ -63,6 +64,8 @@ app.get('/api/checkpwned', async (req, res) => {
   res.send(breachRes);
 });
 
+// STORE NEW PASSWORD INFO ***********
+
 app.post('/api/newpass', async (req, res) => {
   if (req.body.publicKey) {
     var encryptBool = true;
@@ -74,7 +77,7 @@ app.post('/api/newpass', async (req, res) => {
     var encryptBool = false;
   }
 
-  // Pwned Password Breaches
+  // get pwned password breaches
   const hash = crypto.createHash('sha1');
   hash.update(req.body.password);
   var hashedPassword = hash.digest('hex').toUpperCase();
@@ -114,7 +117,8 @@ app.post('/api/newpass', async (req, res) => {
       console.log(error);
     });
 
-  // Strength Score
+  // get password strength
+
   var pass = req.body.password;
   var zxcvbnResult = zxcvbn(pass);
   var strengthScore = zxcvbnResult.score;
@@ -141,31 +145,28 @@ app.post('/api/newpass', async (req, res) => {
   res.send(success);
 });
 
+// GET SINGLE PASSWORD INFO***************
+
 app.get('/api/passinfo', (req, res) => {
   var storeKey = req.body.passwordFor;
   var value = myCache.get(storeKey);
   if (value == undefined) {
-    console.log("CAN'T FIND IT YALLL!!!");
+    console.log("Can't find password info");
   } else {
     var encrypted = value.password;
-    console.log('VALUE', value);
-    console.log('VALUE.ENCRYPTED', value.encrypted);
-    console.log('VALUE.PRIVATEKEY', req.body.privateKey);
     if (req.body.privateKey) {
-      console.log('PRIV IF ONE');
       if (value.encrypted) {
-        console.log('ENCRYPT IF TWO');
         var decryptBuffer = Buffer.from(encrypted.toString('base64'), 'base64');
-        console.log(decryptBuffer);
         var priv = req.body.privateKey.toString();
         var decrypted = privateDecrypt(priv, decryptBuffer);
-        console.log(decrypted);
         value.password = decrypted.toString();
       }
     }
   }
   res.send(value);
 });
+
+// GET LIST OF ALL PASSWORDS INFO***********
 
 app.get('/api/listpass', (req, res) => {
   var passArray = [];
@@ -186,12 +187,21 @@ app.get('/api/listpass', (req, res) => {
   res.send(passArray);
 });
 
+// CHECK STRENGTH OF SINGLE PASSWORD*****
+
 app.get('/api/strength', (req, res) => {
-  var pass = req.body.password;
+  var pass = req.query.password;
   var zxcvbnResult = zxcvbn(pass);
+  var crackTimes = zxcvbnResult.crack_times_display;
   var strengthScore = zxcvbnResult.score;
-  res.send(200, strengthScore);
+  var strength = {
+    score: strengthScore,
+    crackTimes: crackTimes
+  };
+  res.send(200, strength);
 });
+
+// GENERATE PUBLIC/PRIVATE KEYS*********
 
 app.get('/api/genKeys', (req, res) => {
   const { publicKey, privateKey } = generateKeyPairSync('rsa', {
