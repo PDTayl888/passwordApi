52437c288d8b9ed7b8dbb2eb9203e14e96e47774
pdtayl888 <patrickdtaylor1980@gmail.com>
2019-12-17 21:57:13 -0500

    some succesful user tests listpass and passinfo

--- index.js
+++ index.js
@@ -15,19 +15,16 @@ var cors = require('cors');
 app.options('*', cors());
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded());
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-// ////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
 app.get('/api/checkpwned', async (req, res) => {
   const hash = crypto.createHash('sha1');
-  hash.update('red pony');
+  hash.update(req.body.password);
   var hashedPassword = hash.digest('hex').toUpperCase();
-  console.log(hashedPassword);
   var prefix = hashedPassword.slice(0, 5);
-  console.log(prefix);
   var pwnedApi = `https://api.pwnedpasswords.com/range/${prefix}`;
 
   var fullListOfHashes = '';
@@ -66,8 +63,6 @@ app.get('/api/checkpwned', async (req, res) => {
   res.send(breachRes);
 });
 
-// ////////////////////////////////////////////////////////////////
-
 app.post('/api/newpass', async (req, res) => {
   if (req.body.publicKey) {
     var encryptBool = true;
@@ -75,18 +70,15 @@ app.post('/api/newpass', async (req, res) => {
     var encryptBuffer = Buffer.from(toEncrypt);
 
     var encrypted = publicEncrypt(req.body.publicKey, encryptBuffer);
-    console.log('ENCRYPTED', encrypted);
   } else {
     var encryptBool = false;
   }
 
-  //////////////////////////////////////////////
+  // Pwned Password Breaches
   const hash = crypto.createHash('sha1');
   hash.update(req.body.password);
   var hashedPassword = hash.digest('hex').toUpperCase();
-  console.log(hashedPassword);
   var prefix = hashedPassword.slice(0, 5);
-  console.log(prefix);
   var pwnedApi = `https://api.pwnedpasswords.com/range/${prefix}`;
 
   var fullListOfHashes = '';
@@ -121,44 +113,55 @@ app.post('/api/newpass', async (req, res) => {
     .catch(error => {
       console.log(error);
     });
-  ///////////////////////////////////////////////////
+
+  // Strength Score
   var pass = req.body.password;
   var zxcvbnResult = zxcvbn(pass);
   var strengthScore = zxcvbnResult.score;
-  console.log('STRENGTH SCORE', strengthScore);
-
-  ///////////////////////////////////////////////////
-
-  var obj = {
-    passwordFor: req.body.passwordFor,
-    usernameOrEmail: req.body.usernameOrEmail,
-    password: encrypted,
-    strength: strengthScore,
-    pwnedInfo: breachRes,
-    encrypted: encryptBool
-  };
-  console.log('OBJ', obj);
+  if (encryptBool) {
+    var obj = {
+      passwordFor: req.body.passwordFor,
+      usernameOrEmail: req.body.usernameOrEmail,
+      password: encrypted,
+      strength: strengthScore,
+      pwnedInfo: breachRes,
+      encrypted: encryptBool
+    };
+  } else {
+    var obj = {
+      passwordFor: req.body.passwordFor,
+      usernameOrEmail: req.body.usernameOrEmail,
+      password: req.body.password,
+      strength: strengthScore,
+      pwnedInfo: breachRes,
+      encrypted: encryptBool
+    };
+  }
   var success = myCache.set(`${req.body.passwordFor}`, obj, 999999999);
-  console.log('SUCCESS', success);
-  console.log('NEW PASS SUCCESS');
   res.send(success);
 });
 
 app.get('/api/passinfo', (req, res) => {
-  console.log('PASS INFO PASSWORDFOR', req.body.passwordFor);
   var storeKey = req.body.passwordFor;
   var value = myCache.get(storeKey);
   if (value == undefined) {
     console.log("CAN'T FIND IT YALLL!!!");
   } else {
     var encrypted = value.password;
-    if (value.encrypted) {
-      var decryptBuffer = Buffer.from(encrypted.toString('base64'), 'base64');
-      console.log(decryptBuffer);
-      var priv = req.body.privateKey.toString();
-      var decrypted = privateDecrypt(priv, decryptBuffer);
-      console.log(decrypted);
-      value.password = decrypted.toString();
+    console.log('VALUE', value);
+    console.log('VALUE.ENCRYPTED', value.encrypted);
+    console.log('VALUE.PRIVATEKEY', req.body.privateKey);
+    if (req.body.privateKey) {
+      console.log('PRIV IF ONE');
+      if (value.encrypted) {
+        console.log('ENCRYPT IF TWO');
+        var decryptBuffer = Buffer.from(encrypted.toString('base64'), 'base64');
+        console.log(decryptBuffer);
+        var priv = req.body.privateKey.toString();
+        var decrypted = privateDecrypt(priv, decryptBuffer);
+        console.log(decrypted);
+        value.password = decrypted.toString();
+      }
     }
   }
   res.send(value);
@@ -173,26 +176,20 @@ app.get('/api/listpass', (req, res) => {
       if (pass.encrypted) {
         var encrypted = pass.password;
         var decryptBuffer = Buffer.from(encrypted.toString('base64'), 'base64');
-        console.log(decryptBuffer);
         var priv = req.body.privateKey.toString();
         var decrypted = privateDecrypt(priv, decryptBuffer);
-        console.log(decrypted);
         pass.password = decrypted.toString();
       }
     }
     passArray.push(pass);
   });
-  console.log(passArray);
-  console.log(mykeys);
   res.send(passArray);
 });
 
 app.get('/api/strength', (req, res) => {
-  console.log('strength route activated');
-  var pass = 'gary panda deer lambda';
+  var pass = req.body.password;
   var zxcvbnResult = zxcvbn(pass);
   var strengthScore = zxcvbnResult.score;
-  console.log(strengthScore);
   res.send(200, strengthScore);
 });
 
@@ -208,36 +205,12 @@ app.get('/api/genKeys', (req, res) => {
       format: 'pem'
     }
   });
-
-  // print out the generated keys
-  console.log(`PublicKey: ${publicKey}`);
-  console.log(`PrivateKey: ${privateKey}`);
-
-  //message to be encrypted
-  var toEncrypt = 'red pony yellow giraffe boat';
-  var encryptBuffer = Buffer.from(toEncrypt);
-
-  //encrypt using public key
-  var encrypted = publicEncrypt(publicKey, encryptBuffer);
-
-  //print out the text and cyphertext
-  console.log('Text to be encrypted:');
-  console.log(toEncrypt);
-  console.log('cipherText:');
-  console.log(encrypted.toString());
-
-  //decrypt the cyphertext using the private key
-  var decryptBuffer = Buffer.from(encrypted.toString('base64'), 'base64');
-  var decrypted = privateDecrypt(privateKey, decryptBuffer);
-
-  //print out the decrypted text
-  console.log('decrypted Text:');
-  console.log(decrypted.toString());
+  console.log(publicKey);
+  console.log(privateKey);
 
   var keys = {
     public: publicKey,
     private: privateKey
   };
-  console.log(keys.public);
-  res.send(keys.public);
+  res.send(keys);
 });
